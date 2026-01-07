188b5133a9a5b3f63489cf3a691392ca3550c6a6
Jacomo Corrieri <jacomocorrieri@gmail.com>
2026-01-06 21:00:47 -0500

    set working plugins for nvim 0.11.4

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,5 @@
 return {
   'nvim-telescope/telescope.nvim',
-  tag = '0.1.5',  -- Compatible with 0.9.5
   dependencies = { 'nvim-lua/plenary.nvim' },
   keys = {
     { '<leader>ff', '<cmd>Telescope find_files<cr>', desc = 'Find files' },
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  version = "v0.9.2",
+  branch = "master",
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
