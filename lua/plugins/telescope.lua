3b3ac638fd965358597905f7c4a9a72684aeb9d4
Jacomo Corrieri <jacomocorrieri@gmail.com>
2026-01-12 13:38:08 -0500

    add find hidden (all) files cmd

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,8 +1,22 @@
 return {
     'nvim-telescope/telescope.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+        defaults = {
+            file_ignore_patterns = {
+                "node_modules",
+                ".git/",
+                "miniconda3/",
+                "%.pyc$",
+                "__pycache__",
+            }
+        }
+    },
     keys = {
         { '<leader>ff', '<cmd>Telescope find_files<cr>', desc = 'Find files' },
+        { '<leader>fa', function()
+            require('telescope.builtin').find_files({ hidden = true })
+        end, desc = 'Find files w/ hidden' },
         { '<leader>fc', function()
             require('telescope.builtin').find_files({ cwd = vim.fn.stdpath('config') })
         end, desc = 'Find config files' },
@@ -10,6 +24,6 @@ return {
             require('telescope.builtin').find_files({
                 cwd = vim.fn.input("Search in directory: ", vim.fn.getcwd(), "dir"),
             })
-        end, desc = 'Search in given directory' },
+            end, desc = 'Search in given directory' },
     },
 }
