e1e2eef0cada81cda67b9bfec9177c9327a19c2d
Jacomo Corrieri <jacomocorrieri@gmail.com>
2026-01-15 15:31:14 -0500

    enable transparency for onedark

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,12 @@
 return {
-  "navarasu/onedark.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require('onedark').setup({
-      style = 'dark',  -- Options: 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer'
-    })
-    require('onedark').load()
-  end,
+    "navarasu/onedark.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require('onedark').setup({
+            style = 'dark',  -- Options: 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer'
+            transparent = true,
+        })
+        require('onedark').load()
+    end,
 }
