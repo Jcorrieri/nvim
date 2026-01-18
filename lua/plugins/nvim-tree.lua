47e1ee56f8d778c92943f1cd2b81dfb49a04851f
Jacomo Corrieri <jacomocorrieri@gmail.com>
2026-01-18 12:03:20 -0500

    save current settings

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -3,13 +3,13 @@ return {
     "nvim-tree/nvim-tree.lua",
     lazy = false;
     dependencies = {
-        "nvim-tree/nvim-web-devicons", 
+        "nvim-tree/nvim-web-devicons",
     },
     keys = {
         { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Toggle file tree" },
     },
     opts = {
-        sync_root_with_cwd = true,
+        --        sync_root_with_cwd = true,
         update_focused_file = {
             enable = true,
             update_root = true,
