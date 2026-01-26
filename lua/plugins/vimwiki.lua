5b00a43d91cca6a6867c1ae1b2dc55cd7f442003
Jacomo Corrieri <jacomocorrieri@gmail.com>
2026-01-25 22:38:01 -0500

    disable spellcheck on wiki

--- lua/plugins/vimwiki.lua
+++ lua/plugins/vimwiki.lua
@@ -17,6 +17,7 @@ return {
                 vim.opt_local.wrap = true
                 vim.opt_local.linebreak = true
                 vim.opt_local.breakindent = true
+                vim.opt_local.spell = false
             end,
         })
     end,
